bfafae33e28f2c555184f17a6480b9298f507cb7
Крючков Артём Сергеевич <jerrygacket@gmail.com>
2024-07-26 23:04:49 +0300

    search notes

--- src/App.vue
+++ src/App.vue
@@ -9,16 +9,24 @@
           <messageComponent v-if="message" :message="message" />
 
           <newNote :note="newNote" @addNote="addNote" />
-          
+
           <div class="note-header">
+
             <h1>{{ title }}</h1>
+
+            <searchNotes 
+              :value="search" 
+              placeholder="find note" 
+              @search="search = $event"
+            />
+
             <div class="icons">
               <svg :class="{ active: grid }" @click="grid=true" class="btn-remove" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" ><rect x="3" y="3" width="7" height="7"></rect><rect x="14" y="3" width="7" height="7"></rect><rect x="14" y="14" width="7" height="7"></rect><rect x="3" y="14" width="7" height="7"></rect></svg>
               <svg :class="{ active: !grid }" @click="grid=false" class="btn-remove" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="8" y1="6" x2="21" y2="6"></line><line x1="8" y1="12" x2="21" y2="12"></line><line x1="8" y1="18" x2="21" y2="18"></line><line x1="3" y1="6" x2="3" y2="6"></line><line x1="3" y1="12" x2="3" y2="12"></line><line x1="3" y1="18" x2="3" y2="18"></line></svg>
             </div>
           </div>
 
-          <notesList :notes="notes" :grid="grid" @remove="removeNote" />
+          <notesList :notes="notesFilter" :grid="grid" @remove="removeNote" />
         </div>
       </section>
     </div>
@@ -30,17 +38,20 @@
 import messageComponent from '@/components/MessageComponent.vue'
 import newNote from '@/components/NewNote.vue'
 import notesList from '@/components/NotesList.vue'
+import searchNotes from '@/components/SearchNotes.vue'
 export default {
   components: {
     messageComponent,
     newNote,
-    notesList
+    notesList,
+    searchNotes
   },
   data () {
     return {
       title: 'notes app',
       message: null,
       grid: true,
+      search: '',
       newNote: {
               title: '',
               descr: ''
@@ -89,6 +100,22 @@ export default {
       removeNote (id) {
         this.notes.splice(id, 1);
       }
+  },
+  computed: {
+    notesFilter () {
+      let arr = this.notes,
+        search = this.search;
+      if (!search) return arr
+
+      search = search.trim().toLowerCase();
+
+      arr = arr.filter(function(item) {
+        if (item.title.toLowerCase().indexOf(search) !== -1)
+          return item
+      })
+
+      return arr;
+    }
   }
 }
 </script>
